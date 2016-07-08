3d0a20d1cf537199ae6188491a03d5a2bb73dd9d
AparnaAbi <aparna8696@gmail.com>
2016-07-08 11:09:02 +0530

    finish

---
+++ largest.c
@@ -0,0 +1,14 @@
+# lar
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter the numbers:");
+scanf("%d%d%d",&a,&b,&c);
+if((a>b)&&(a>c))
+printf("a is largest");
+else if(b>c)
+printf("b is largest");
+else
+printf("c is largest");
+}
